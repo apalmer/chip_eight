31f93f48111560f9f3c9b6ab2e9d4b03390cca18
Adigun Palmer <adigunpalmer@hotmail.com>
2021-01-20 15:19:30 -0500

    fixed pytest issues

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,7 +3,7 @@ import chip_eight
 class TestMachine:
     def test_load(capsys):
         ordered = bytearray([i for i in range(0x00,0x100,0x01)])
-        sut = Machine()
+        sut = chip_eight.Machine()
         sut.initialize()
         sut.loadRom(ordered)        
         actual = sut.process_operation()
